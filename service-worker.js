0bd50f97e9b2931da9b5de8bbcb929980b16382b
Rodrigo Deodoro <rodrigo.deodoro@esharesinc.com>
2019-02-27 12:23:49 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stophubby/precache-manifest.ae84e177cb5721f83646ed19521725b0.js"
+  "/precache-manifest.dd796ea870d59c4e283d76752d12167a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stophubby/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
