688c37e07f6d5fd2f028857d21fe08c6fe16d0ba
Rodrigo Deodoro <rodrigo.deodoro@esharesinc.com>
2019-02-27 13:47:45 -0800

    Run Prettier

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import Ticker from "./Ticker"
-import sample from 'lodash.sample';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Ticker from "./Ticker";
+import sample from "lodash.sample";
+import "./App.css";
 
 const ALTERNATIVES = [
-  'bro',
-  'dude',
-  'pal',
-  'mate',
-  'sir',
-  'husbando',
-  'person who I have married',
-  'buddy',
-  'friendo',
-  'cunt',
-  'this asshole over here',
-  'motherfucker',
-  'the impreginator',
-  'my boyfriend',
-  'Princess Jasmine',
-  'his mother\'s name',
-  'Benedict Cumberbatch',
-  'Big Mac',
-  'Poop emoji',
-  'old man',
-  'his name',
-  'names',
-
+  "bro",
+  "dude",
+  "pal",
+  "mate",
+  "sir",
+  "husbando",
+  "person who I have married",
+  "buddy",
+  "friendo",
+  "cunt",
+  "this asshole over here",
+  "motherfucker",
+  "the impreginator",
+  "my boyfriend",
+  "Princess Jasmine",
+  "his mother's name",
+  "Benedict Cumberbatch",
+  "Big Mac",
+  "Poop emoji",
+  "old man",
+  "his name",
+  "names"
 ];
 
-
-const App = (props) => {
+const App = props => {
   let options = [...ALTERNATIVES];
 
-  const [text, setText] = useState(sample(options))
+  const [text, setText] = useState(sample(options));
 
-  useEffect(
-    () => {
-      const timer = window.setInterval(() => {
-        if (!options.length) {
-          options = [...ALTERNATIVES];
-        }
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      if (!options.length) {
+        options = [...ALTERNATIVES];
+      }
 
-        setText(sample(options));
-      }, 3000);
+      setText(sample(options));
+    }, 3000);
 
-      return () => window.clearInterval(timer);
-    }, []
-  );
+    return () => window.clearInterval(timer);
+  }, []);
 
   return (
     <div className="App">
@@ -57,12 +52,12 @@ const App = (props) => {
 
       <h2>HERE'S WHAT YOU CAN CALL HIM INSTEAD:</h2>
       <h1>
-        <Ticker text={ text }/>
+        <Ticker text={text} />
       </h1>
 
       <h3>NO YOU CAN'T CALL HIM DADDY EITHER</h3>
     </div>
-  )
-}
+  );
+};
 
 export default App;
