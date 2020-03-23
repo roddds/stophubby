9454c0d013978916bfaf51e535388525a0a76ff7
Rodrigo Deodoro <roddds@gmail.com>
2020-03-23 13:10:18 -0700

    Fix height; prettier

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from "react";
-import Ticker from "./Ticker";
-import shuffle from "lodash.shuffle";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Ticker from './Ticker';
+import shuffle from 'lodash.shuffle';
+import './App.css';
 
 const ALTERNATIVES = [
   // DON'T BOTHER WITH TITLE CASE
   // EVERYTHING WILL BE DISPLAYED AS UPPERCASE
-  "bro",
-  "dude",
-  "pal",
-  "mate",
-  "sir",
-  "husbando",
-  "person who i have married",
-  "buddy",
-  "friendo",
-  "this asshole over here",
-  "the impreginator",
-  "my boyfriend",
-  "princess jasmine",
+  'bro',
+  'dude',
+  'pal',
+  'mate',
+  'sir',
+  'husbando',
+  'person who i have married',
+  'buddy',
+  'friendo',
+  'this asshole over here',
+  'the impreginator',
+  'my boyfriend',
+  'princess jasmine',
   "his mother's name",
-  "benedict cumberbatch",
-  "big mac",
-  "poop emoji",
-  "old man",
-  "his name",
-  "names",
-  "githubby",
-  "hey dumbass",
-  "fuckhead",
-  "mother hubbard",
-  "prime minister",
-  "my future late husband",
-  "rat bastard",
-  "emergency contact",
-  "bucko",
-  "shitstain",
+  'benedict cumberbatch',
+  'big mac',
+  'poop emoji',
+  'old man',
+  'his name',
+  'names',
+  'githubby',
+  'hey dumbass',
+  'fuckhead',
+  'mother hubbard',
+  'prime minister',
+  'my future late husband',
+  'rat bastard',
+  'emergency contact',
+  'bucko',
+  'shitstain'
 ];
 
+const list = shuffle([...ALTERNATIVES]);
 
 const App = props => {
-  const list = shuffle([...ALTERNATIVES]);
   const [index, setIndex] = useState(0);
 
   const currentText = list[(index + list.length) % list.length];
@@ -54,21 +54,21 @@ const App = props => {
   }, [index]);
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>STOP CALLING YOUR HUSBANDS HUBBY</h1>
       <h3>HOLY FUCK</h3>
 
       <h2>HERE'S WHAT YOU CAN CALL HIM INSTEAD:</h2>
       <h1>
-        <Ticker text={ currentText } />
+        <Ticker text={currentText} />
       </h1>
 
       <h3>NO YOU CAN'T CALL HIM DADDY EITHER</h3>
 
       <small>
-        Made with hate by <a href="https://github.com/roddds">@roddds</a> with
-        contributions from <a href="https://github.com/jessapp">@jessapp</a> and
-        { ' ' }<a href="https://github.com/norasf">@norasf</a>.
+        Made with hate by <a href='https://github.com/roddds'>@roddds</a> with
+        contributions from <a href='https://github.com/jessapp'>@jessapp</a> and{' '}
+        <a href='https://github.com/norasf'>@norasf</a>.
       </small>
     </div>
   );
