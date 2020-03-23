ebbe86c776d12bae7bed722758277b58fe36cdf9
Rodrigo Deodoro <roddds@gmail.com>
2020-03-23 13:10:44 -0700

    [skip ci] Updates

---
+++ precache-manifest.35bb99cc4536edf735b2471ef0bdcdbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6083d62263bc4e29b2f15514ea803812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1829bd3a5801b124380f",
+    "url": "/static/css/main.b8666ae4.chunk.css"
+  },
+  {
+    "revision": "d6b964034c8aed4e78af",
+    "url": "/static/js/2.39813fdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39813fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1829bd3a5801b124380f",
+    "url": "/static/js/main.b69e6c4e.chunk.js"
+  },
+  {
+    "revision": "d1f669ea307d7cbbaa68",
+    "url": "/static/js/runtime-main.e356b1c7.js"
+  }
+]);
