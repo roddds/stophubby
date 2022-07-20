1b2435b11f4785beb639e4d978cc8ade83b1ea87
CircleCI <ci-build@deodoro.org>
2022-07-20 03:42:57 +0000

    [skip ci] Updates

---
+++ precache-manifest.41e53b94d6c8e6513d90c2c402257e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df74cc44fdca4ff455b112c5384ded6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a867362566618f09187",
+    "url": "/static/css/main.f8cb30cd.chunk.css"
+  },
+  {
+    "revision": "d4677b0fa11d996c9ef9",
+    "url": "/static/js/2.10990c48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10990c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a867362566618f09187",
+    "url": "/static/js/main.a776514d.chunk.js"
+  },
+  {
+    "revision": "ec7451ff7208f0949521",
+    "url": "/static/js/runtime-main.8bb5a81f.js"
+  }
+]);
